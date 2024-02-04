01362f8296ffda1bff44a97fc60938fcb918df10
vaibhav-dawane <vaibhavdawane07@gmail.com>
2024-02-04 18:39:16 +0530

    added notification ask alert whenever, user opens web app

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function startReminders(type) {
     } else if (type === 'rest') {
         restInterval = setInterval(function() {
             showReminderNotification('It\'s time to take a break and rest!');
-        }, 1 * 60 * 1000); // Rest reminder every 60 minutes
+        }, 60 * 60 * 1000); // Rest reminder every 60 minutes
     }
 
     alert(type.charAt(0).toUpperCase() + type.slice(1) + ' reminders started!');
@@ -41,3 +41,14 @@ function stopReminders(type) {
 
     alert(type.charAt(0).toUpperCase() + type.slice(1) + ' reminders stopped!');
 }
+
+// Request notification permission when the web app is opened
+document.addEventListener('DOMContentLoaded', function() {
+    if ('Notification' in window) {
+        Notification.requestPermission().then(function(permission) {
+            if (permission !== 'granted') {
+                console.log('Notification permission denied.');
+            }
+        });
+    }
+});
