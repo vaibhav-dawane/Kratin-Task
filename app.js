46f016916d11ce7959cc02f0bd6c03f4c65ccf9b
vaibhav-dawane <vaibhavdawane07@gmail.com>
2024-02-04 18:33:00 +0530

    added take rest reminder

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-let reminderInterval;
+let exerciseInterval;
+let restInterval;
 
-function showExerciseNotification() {
+function showReminderNotification(message) {
     if ('Notification' in window) {
         Notification.requestPermission().then(function(permission) {
             if (permission === 'granted') {
-                const notification = new Notification('Exercise Reminder', {
-                    body: 'It\'s time to take a short walk or do some exercises!',
+                const notification = new Notification('Wellness Reminder', {
+                    body: message,
                 });
 
                 notification.onclick = function() {
@@ -17,13 +18,26 @@ function showExerciseNotification() {
     }
 }
 
-function startReminders() {
-    // Set a reminder every 30 minutes (adjust as needed)
-    reminderInterval = setInterval(showExerciseNotification, 1 * 60 * 1000);
-    alert('Reminders started!');
+function startReminders(type) {
+    if (type === 'exercise') {
+        exerciseInterval = setInterval(function() {
+            showReminderNotification('It\'s time to take a short walk or do some exercises!');
+        }, 30 * 60 * 1000); // Exercise reminder every 30 minutes
+    } else if (type === 'rest') {
+        restInterval = setInterval(function() {
+            showReminderNotification('It\'s time to take a break and rest!');
+        }, 1 * 60 * 1000); // Rest reminder every 60 minutes
+    }
+
+    alert(type.charAt(0).toUpperCase() + type.slice(1) + ' reminders started!');
 }
 
-function stopReminders() {
-    clearInterval(reminderInterval);
-    alert('Reminders stopped!');
+function stopReminders(type) {
+    if (type === 'exercise') {
+        clearInterval(exerciseInterval);
+    } else if (type === 'rest') {
+        clearInterval(restInterval);
+    }
+
+    alert(type.charAt(0).toUpperCase() + type.slice(1) + ' reminders stopped!');
 }
